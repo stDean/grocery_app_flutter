96db3b4529f3980888d3af50b1d062e700b7c91e
Toba-dean <tobaogundimu@gmail.com>
2024-05-27 20:21:21 +0100

    using the provider to pass state, and added the grocery list items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:first/model/cart_model.dart';
 import 'package:first/pages/Intro.page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroPage(),
+      ),
     );
   }
 }
--- lib/pages/Home.page.dart
+++ lib/pages/Home.page.dart
@@ -1,7 +1,9 @@
-import 'package:first/component/GroceryItem.dart';
+import 'package:first/component/grocery_item_tile.dart';
+import 'package:first/model/cart_model.dart';
 import 'package:first/pages/Cart.page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -104,13 +106,29 @@ class HomePage extends StatelessWidget {
 
           // products
           Expanded(
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1 / 1.2,
-              ),
-              itemBuilder: (context, index) {
-                return const GroceryItemTile();
+            child: Consumer<CartModel>(
+              builder: (context, value, child) {
+                return GridView.builder(
+                  padding: const EdgeInsets.all(12),
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: value.shopItems.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1 / 1.2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return  GroceryItemTile(
+                      itemName: value.shopItems[index][0],
+                      itemPrice: value.shopItems[index][1],
+                      imagePath: value.shopItems[index][2],
+                      color: value.shopItems[index][3],
+                      // onPressed: () =>
+                      //     Provider.of<CartModel>(context, listen: false)
+                      //         .addItemToCart(index),
+                      onPressed: null,
+                    );
+                  },
+                );
               },
             ),
           )
