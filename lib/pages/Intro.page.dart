c622b8ebb50a69e5aac31fa1230585f5adc9443d
Toba-dean <tobaogundimu@gmail.com>
2024-05-27 15:26:19 +0100

    added the landing screen which leads to the home page

--- lib/pages/Intro.page.dart
+++ lib/pages/Intro.page.dart
@@ -1,10 +1,84 @@
+import 'package:first/pages/Home.page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: SafeArea(
+        child: Column(
+          children: [
+            // logo
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 100.0,
+                right: 100.0,
+                top: 120,
+                bottom: 20,
+              ),
+              child: Image.asset('lib/images/avocado.png'),
+            ),
+
+            // we deliver groceries at your doorstep
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Text('We deliver groceries at your doorstep',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.notoSerif(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+
+            // groceries gives you fresh vegetables and fruits
+            Text(
+              'Fresh items everyday',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            const Spacer(),
+
+            // Get started button
+            GestureDetector(
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const HomePage();
+                  },
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 112, 91, 222),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding: const EdgeInsets.all(24.0),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
   }
 }
