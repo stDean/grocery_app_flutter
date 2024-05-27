389c45e5fc6f670e3a558ca23d01c324627259d8
Toba-dean <tobaogundimu@gmail.com>
2024-05-27 15:50:10 +0100

    started added the widgets for the home page

--- lib/pages/Home.page.dart
+++ lib/pages/Home.page.dart
@@ -1,12 +1,121 @@
+import 'package:first/component/GroceryItem.dart';
+import 'package:first/pages/Cart.page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Home Page'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: Icon(
+            Icons.location_on,
+            color: Colors.grey[700],
+          ),
+        ),
+        title: Text(
+          'Sydney, Australia',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey[700],
+          ),
+        ),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const CartPage();
+            },
+          ),
+        ),
+        child: const Icon(Icons.shopping_bag),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 48),
+
+          // good morning bro
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text('Good morning,'),
+          ),
+
+          const SizedBox(height: 4),
+
+          // Let's order fresh items for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Let's order fresh items for you",
+              style: GoogleFonts.notoSerif(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 24),
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Divider(),
+          ),
+
+          const SizedBox(height: 24),
+
+          // categories -> horizontal listview
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Fresh Items",
+              style: GoogleFonts.notoSerif(
+                //fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+
+          // products
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1 / 1.2,
+              ),
+              itemBuilder: (context, index) {
+                return const GroceryItemTile();
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
